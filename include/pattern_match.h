54ea7e34a0a6417708c2a25677075e7720f46420
Chandan Reddy <chandan1986g@gmail.com>
2019-05-02 16:32:11 +0200

    Refactor

---
+++ include/pattern_match.h
@@ -0,0 +1,79 @@
+#include <islutils/builders.h>
+#include <islutils/ctx.h>
+#include <islutils/matchers.h>
+#include <islutils/pet_wrapper.h>
+#include <limits>
+
+#include <pet.h>
+
+
+enum class PatternType { Gemm, MatrixVector, DotProduct, None };
+
+std::ostream &operator<<(std::ostream &os, PatternType p);
+
+class Pattern {
+public:
+  Pattern() : type_(PatternType::None) {}
+  Pattern(PatternType t) : type_(t) {}
+  virtual bool isMatching(isl::schedule_node node) const { return false; }
+  virtual int Cost() const { return 0; }
+  PatternType GetType() const { return type_; }
+
+private:
+  PatternType type_;
+};
+
+
+class GemmPattern : public Pattern {
+public:
+  GemmPattern() : Pattern(PatternType::Gemm) {}
+
+  virtual bool isMatching(isl::schedule_node node) const override {
+
+    return matchers::ScheduleNodeMatcher::isMatching(matcher_, node);
+  }
+
+  virtual int Cost() const override { return MatchingCost; }
+
+private:
+  static constexpr int MatchingCost = 3;
+  static matchers::ScheduleNodeMatcher matcher_ ;
+};
+
+class MatrixVectorPattern : public Pattern {
+public:
+  MatrixVectorPattern() : Pattern(PatternType::MatrixVector) {}
+
+  virtual bool isMatching(isl::schedule_node node) const override {
+
+    return matchers::ScheduleNodeMatcher::isMatching(matcher_, node);
+  }
+
+  virtual int Cost() const override { return MatchingCost; }
+
+private:
+  static constexpr int MatchingCost = 20;
+  static matchers::ScheduleNodeMatcher matcher_ ;
+};
+
+class DotProductPattern : public Pattern {
+public:
+  DotProductPattern() : Pattern(PatternType::DotProduct) {}
+
+  virtual bool isMatching(isl::schedule_node node) const override {
+
+    return matchers::ScheduleNodeMatcher::isMatching(matcher_, node);
+  }
+
+  virtual int Cost() const override { return MatchingCost; }
+
+private:
+  static constexpr int MatchingCost = 1;
+  static matchers::ScheduleNodeMatcher matcher_ ;
+};
+
+typedef std::unique_ptr<Pattern> UPattern;
+std::vector<UPattern> AllPatterns();
+
+int MatchPatterns(isl::schedule_node node,
+                  std::vector<PatternType> &matchedPatterns);
---
+++ src/pattern_match.cc
@@ -0,0 +1,90 @@
+#include "pattern_match.h"
+
+using namespace matchers;
+
+std::ostream &operator<<(std::ostream &os, PatternType p) {
+  switch (p) {
+  case PatternType::Gemm:
+    return os << "GEMM";
+  case PatternType::MatrixVector:
+    return os << "Matrix Vector";
+  case PatternType::DotProduct:
+    return os << "Dot Product";
+  default:
+    return os << "None";
+  };
+}
+
+  // clang-format off
+  ScheduleNodeMatcher GemmPattern::matcher_ = 
+    band(
+      band(
+        sequence(
+          filter(
+            leaf()),
+          filter(
+            band(
+              leaf())))));
+
+  ScheduleNodeMatcher MatrixVectorPattern::matcher_ = 
+      band(
+        sequence(
+          filter(
+            leaf()),
+          filter(
+            band(
+              leaf()))));
+
+  ScheduleNodeMatcher DotProductPattern::matcher_ = 
+        sequence(
+          filter(
+            leaf()),
+          filter(
+            band(
+              leaf())));
+  // clang-format on
+
+std::vector<UPattern> AllPatterns() {
+
+  std::vector<UPattern> patterns;
+  patterns.emplace_back(std::make_unique<GemmPattern>());
+  patterns.emplace_back(std::make_unique<MatrixVectorPattern>());
+  patterns.emplace_back(std::make_unique<DotProductPattern>());
+
+  return patterns;
+}
+
+int MatchPatterns(isl::schedule_node node,
+                  std::vector<PatternType> &matchedPatterns) {
+
+  int max_cost = std::numeric_limits<int>::min();
+  matchedPatterns.push_back(PatternType::None);
+
+  // match the current node, top level pattern matches
+  // are prioritized
+  for (auto const &p : AllPatterns()) {
+    if (p->isMatching(node)) {
+      int cur_cost = p->Cost();
+      if (max_cost < cur_cost) {
+        max_cost = cur_cost;
+        matchedPatterns.pop_back();
+        matchedPatterns.push_back(p->GetType());
+      }
+    }
+  }
+
+  // try matching all subtrees 
+  for (int i = 0; i < node.n_children(); ++i) {
+    std::vector<PatternType> subtreePatterns;
+    int cur_cost = MatchPatterns(node.child(i), subtreePatterns);
+    if (max_cost < cur_cost) {
+      max_cost = cur_cost;
+
+      // Remove previous match
+      matchedPatterns.pop_back();
+      matchedPatterns.push_back(subtreePatterns[0]);
+    }
+  }
+
+  return max_cost;
+}
